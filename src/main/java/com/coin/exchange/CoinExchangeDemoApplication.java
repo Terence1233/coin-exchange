5ba45727f22898cd594b6c6bc18af6eaa333422b
zbq <s1105775949@163.com>
2021-12-13 15:39:17 +0800

    first commit

---
+++ src/main/java/com/coin/exchange/CoinExchangeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.coin.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinExchangeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinExchangeDemoApplication.class, args);
+    }
+
+}
